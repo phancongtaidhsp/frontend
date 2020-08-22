47e9992a7b7c91295dc06d5d5ef9272897d37a1d
phancongtaidhsp <phancongtaicntt2@gmail.com>
2020-08-22 22:14:42 +0700

    push origin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AdminPage from './pages/adminpage.component'
 import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage.component';
 
 function App() {
   return (
     <Switch>
+      <Route path="/" component={HomePage} />
       <Route path="/admin" component={AdminPage} />
     </Switch>
   );
